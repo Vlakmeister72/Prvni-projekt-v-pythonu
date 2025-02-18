d411116e6a507a13e394c10f3dfa1717fb972297
Matyáš Bláha <mates8blaha@seznam.cz>
2025-02-18 15:04:43 +0100

    3. Hodina

---
+++ input.py
@@ -0,0 +1,72 @@
+sklenice1 = "Mlíko - Herdyn"
+sklenice2 = "Pivo"
+sklenice3 = ""
+
+
+print(f"Máme dvě sklenice, V jedný je {sklenice1} a v druhý {sklenice2}.")
+print(f"Vytáhneme třetí sklenici.")
+print(f"Sklenice3 = {sklenice3}")
+print(f"Sklenice2 = {sklenice2}")
+print(f"Sklenice1 = {sklenice1}")
+
+sklenice3 = sklenice2
+sklenice2 = ""
+
+print(f"Přelejeme {sklenice3} do prázndé sklenice")
+print(f"Sklenice3 = {sklenice3}")
+print(f"Sklenice2 = {sklenice2}")
+print(f"Sklenice1 = {sklenice1}")
+
+sklenice2 = sklenice1
+sklenice1 = ""
+print(f"Přelejeme {sklenice2} do vyprázdněné sklenice2")
+print(f"Sklenice3 = {sklenice3}")
+print(f"Sklenice2 = {sklenice2}")
+print(f"Sklenice1 = {sklenice1}")
+
+sklenice1 = sklenice3
+sklenice3 = ""
+print(f"Přelejeme {sklenice1} do vyprázdněné sklenice1")
+print(f"Sklenice3 = {sklenice3}")
+print(f"Sklenice2 = {sklenice2}")
+print(f"Sklenice1 = {sklenice1}")
+
+print("A je hotovo!")
+
+#Teď budem pracovat s inputem.
+sklenice1 = input("Zadejte název nápoje: ")
+sklenice2 = input("Zadejte název nápoje: ")
+
+print("Přidáme sklenici3")
+
+sklenice3 = ""
+
+print(f"Máme dvě sklenice, V jedný je {sklenice1} a v druhý {sklenice2}.")
+print(f"Vytáhneme třetí sklenici.")
+print(f"Sklenice3 = {sklenice3}")
+print(f"Sklenice2 = {sklenice2}")
+print(f"Sklenice1 = {sklenice1}")
+
+sklenice3 = sklenice2
+sklenice2 = ""
+
+print(f"Přelejeme {sklenice3} do prázndé sklenice")
+print(f"Sklenice3 = {sklenice3}")
+print(f"Sklenice2 = {sklenice2}")
+print(f"Sklenice1 = {sklenice1}")
+
+sklenice2 = sklenice1
+sklenice1 = ""
+print(f"Přelejeme {sklenice2} do vyprázdněné sklenice2")
+print(f"Sklenice3 = {sklenice3}")
+print(f"Sklenice2 = {sklenice2}")
+print(f"Sklenice1 = {sklenice1}")
+
+sklenice1 = sklenice3
+sklenice3 = ""
+print(f"Přelejeme {sklenice1} do vyprázdněné sklenice1")
+print(f"Sklenice3 = {sklenice3}")
+print(f"Sklenice2 = {sklenice2}")
+print(f"Sklenice1 = {sklenice1}")
+
+print("A je hotovo!")
